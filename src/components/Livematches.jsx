e1a0a1b7f0896c08dbc87cb67ef8526111b5d131
Aditya Aggarwal <https://C01188@developer.acs.org/stash/scm/co/cen-redesign.git/>
2018-08-09 03:42:52 +0530

    fetched all matches from API

--- src/components/Livematches.jsx
+++ src/components/Livematches.jsx
@@ -1,12 +1,64 @@
 import React, { Component } from 'react';
 
-export class Livematches extends Component {
+export class Livematches extends Component 
+{
+
+	constructor(props)
+	{
+		super(props);
+		this.state={
+						error: false,
+						isLoaded: false,
+						data: {}
+					};
+	}
+
+	componentDidMount = () =>
+	{
+		fetch("http://cricapi.com/api/matches?apikey=Shoc1nsinOU7YNbpSsGNie3xIlS2")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            data: result.matches
+          });
+        },
+        
+	        (error) => {
+	          this.setState({
+	            isLoaded: true,
+	            error: true
+	          });
+	        }
+	      )
+	}
+
 	render() {
-		return (
-			<div>
-				Live Matches Component
-			</div>
-		);
+
+		if(this.state.error)
+		{
+			return(
+					<div>Error</div>
+				);
+		}
+
+		else if(!this.state.isLoaded)
+		{
+			return(
+					<div>Is loading ...</div>
+				);
+		}
+
+		else
+		{
+			let myData=JSON.stringify(this.state.data);
+			return(
+					<div>{myData}</div>
+				);
+		}
+
+		
 	}
 }
 
